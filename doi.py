03692bad9904088843590482b44eb77e52d9d4e8
yash-dani <daniyash19@gmail.com>
2021-01-01 17:02:17 -0500

    messed around with regex possibilities for doi

--- doi.py
+++ doi.py
@@ -2,6 +2,7 @@
 author @yash
 some utility functions for adding DOI functionality to tl;dr papers
 '''
+import re
 
 
 # uses crossref API to (attempt to) parse an abstract given a DOI
@@ -23,6 +24,18 @@ def getAbstractfromDOI(doi):
 	else:
 		return 1
 
+'''
 # checks validity of DOI input
 def checkDOI(doi):
-	
+	# some regex magic from 
+	# https://www.crossref.org/blog/dois-and-matching-regular-expressions/
+	test_string = 'a1b2cdefg'
+
+	matched = re.match("[a-z][0-9][a-z][0-9]+", test_string)
+	is_match = bool(matched)
+
+	print(is_match)
+	return bool(re.match(r"/^10.\d{4,9}/[-._;()/:A-Z0-9]+$/i", doi))
+
+print(checkDOI('/10.1155/2016/3845247'))
+'''
